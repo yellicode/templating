0cd9a9f68cc761e9ccef3f7d4458d8deac813f23
Maurice de Laat <info@mauricedelaat.nl>
2018-05-25 08:14:15 +0200

    Extended the TypeNameProvider interface to support both Type and TypedElement as function parameter. Updated the DefaultTypeNameProvider implementation.

--- src/type-name-provider.ts
+++ src/type-name-provider.ts
@@ -14,6 +14,11 @@ import * as elements from "@yellicode/elements";
  */
 export interface TypeNameProvider {
     /**
+     * Gets the name of the typed type. If the name could not be determined, this function returns null.
+     */
+    getTypeName(type: elements.Type): string | null;
+    
+    /**
      * Gets the name of the typed element's type. If the name could not be determined, this function returns null.
      */
     getTypeName(typedElement: elements.TypedElement): string | null;
@@ -25,28 +30,55 @@ export interface TypeNameProvider {
  * and other types separately.
  */
 export class DefaultTypeNameProvider implements TypeNameProvider {
-    public getTypeName(typedElement: elements.TypedElement): string | null {
-        if (!typedElement.type) return null;
-        return elements.isDataType(typedElement.type) ? 
-            this.getDataTypeName(typedElement) : 
-            this.getComplexTypeName(typedElement);
+    public getTypeName(type: elements.Type): string | null;
+    public getTypeName(typedElement: elements.TypedElement): string | null;
+    public getTypeName(typeOrTypedElement: elements.Type | elements.TypedElement): string | null {
+        if (elements.isTypedElement(typeOrTypedElement)) {
+            // The argument is a typedElement            
+            return elements.isDataType(typeOrTypedElement.type) ? this.getDataTypeName(typeOrTypedElement) : this.getComplexTypeName(typeOrTypedElement);
+        }
+        else {
+            // The argument is a type
+            return elements.isDataType(typeOrTypedElement) ? this.getDataTypeNameForType(typeOrTypedElement) : this.getComplexTypeNameForType(typeOrTypedElement);
+        }
     }
 
     /**
+     * Returns the name of the provided element's data type. Override this function to map primitives and other data types 
+     * (both built-in or types exported from a profile) to the target language. The default implementation calls the 
+     * getDataTypeNameForType function using the type of the typedElement.
+     * @param typedElement Any TypedElement instance.
+     */
+    protected /*virtual*/ getDataTypeName(typedElement: elements.TypedElement): string | null {
+        return this.getDataTypeNameForType(typedElement.type);
+    }
+
+     /**
      * Returns the name of the provided data type. Override this function to map primitives and other data types 
      * (both built-in or types exported from a profile) to the target language.
      * @param type The type information.
      */
-    protected /*virtual*/ getDataTypeName(typedElement: elements.TypedElement): string | null {
-        return typedElement.getTypeName();
+    protected /*virtual*/ getDataTypeNameForType(type: elements.Type | null): string | null {
+        return type ? type.name : null;
     }
 
     /**
-     * Returns the name of the provided complex type (any type that is not a data). Override this function to provide
+     * Returns the name of the provided element's complex type (any type that is not a DataType). Override this function to provide
+     * a custom name for the complex type. The default implementation calls the getComplexTypeNameForType function using 
+     * the type of the typedElement.
+     * @param typedElement Any TypedElement instance.
+     */
+    protected /*virtual*/ getComplexTypeName(typedElement: elements.TypedElement): string | null {
+        return this.getComplexTypeNameForType(typedElement.type);
+    }
+
+      /**
+     * Returns the name of the provided complex type (any type that is not a DataType). Override this function to provide
      * a custom name for the complex type.
      * @param type The type information.
      */
-    protected /*virtual*/ getComplexTypeName(typedElement: elements.TypedElement): string | null {
-        return typedElement.getTypeName();
+    protected /*virtual*/ getComplexTypeNameForType(type: elements.Type | null): string | null {
+        return type ? type.name : null;
     }
+
 }
